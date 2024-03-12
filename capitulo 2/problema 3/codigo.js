60c4d7f8925bb9231143dd2387466d5f48e0bc3f
Diocco <diegoiocco13@gmail.com>
2024-03-12 08:39:08 -0300

    Optimizacion JavaScript Problema 3 capitulo 2

--- capitulo 2/problema 3/codigo.js
+++ capitulo 2/problema 3/codigo.js
@@ -1,46 +1,29 @@
 
-const sumar = (num1,num2)=>{
-	return parseInt(num1) + parseInt(num2);
+const operacion = (num1,num2,oper)=>{
+	if (oper == 1) {
+		resultado = num1 + num2;
+	}
+	else if (oper == 2) {
+		resultado = num1 - num2;
+	}
+	else if (oper == 3) {
+		resultado = num1 / num2;
+	}
+	else if (oper == 4) {
+		resultado = num1 * num2;
+	}
+	alert(`tu resultado es ${resultado}`);
+	return;
 }
-const restar = (num1,num2)=>{
-	return parseInt(num1) - parseInt(num2);
-}
-const dividir = (num1,num2)=>{
-	return parseInt(num1) / parseInt(num2);
-}
-const multiplicar = (num1,num2)=>{
-	return parseInt(num1) * parseInt(num2);
-}
-
 
 alert("¿Que operación deseas realizar?");
-let operacion = prompt("1: suma,  2: resta,  3: división,  4: multiplicación");
+let oper = parseInt(prompt("1: suma,  2: resta,  3: división,  4: multiplicación"));
+let numero1 = parseInt(prompt("primer número para operar"));
+let numero2 = parseInt(prompt("segundo número para operar"));
 
-if (operacion == 1) {
-	let numero1 = prompt("primer número para sumar");
-	let numero2 = prompt("segundo número para sumar");
-	resultado = sumar(numero1,numero2);
-	alert(`tu resultado es ${resultado}`);
+if (oper==1||oper==2||oper==3||oper==4){
+	operacion(numero1,numero2,oper);
 }
-else if (operacion == 2) {
-	let numero1 = prompt("primer número para restar");
-	let numero2 = prompt("segundo número para restar");
-	resultado = restar(numero1,numero2)
-	alert(`tu resultado es ${resultado}`);
-}
-else if (operacion == 3) {
-	let numero1 = prompt("primer número para dividir");
-	let numero2 = prompt("segundo número para dividir");
-	resultado = dividir(numero1,numero2)
-	alert(`tu resultado es ${resultado}`);
-}
-else if (operacion == 4) {
-	let numero1 = prompt("primer número para multiplicar");
-	let numero2 = prompt("segundo número para multiplicar");
-	resultado = multiplicar(numero1,numero2)
-	alert(`tu resultado es ${resultado}`);
-}
-
 else {
 	alert("no se ha encontrado la operación")
 }
